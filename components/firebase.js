c4399ed5a3c069da4350d8a8e02e085e6c79e894
Ricqua <ricqua@gmail.com>
2021-04-19 01:19:04 +0900

    add emailjs to backorder page

---
+++ components/firebase.js
@@ -0,0 +1,18 @@
+import firebase from "firebase/app";
+import "firebase/auth";
+import "firebase/database";
+
+const firebaseConfig = {
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_PUBLIC_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTHDOMAIN,
+  databaseURL: process.env.NEXT_PUBLIC_FIREBASE_DB_URL,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+};
+
+export default function initFirebase() {
+  if (!firebase.apps.length) {
+    firebase.initializeApp(firebaseConfig);
+  } else {
+    firebase.app(); // if already initialized, use that one
+  }
+}
--- pages/backorder.js
+++ pages/backorder.js
@@ -1,11 +1,55 @@
-import React from "react";
-// import Image from "next/image";
+import React, { useState } from "react";
 import Head from "next/head";
+import emailjs from "emailjs-com";
+import Modal from "react-modal";
 
-// import ProductCard from "../components/ProductCard";
-// import { biltongBags } from "../data/productData";
+// import firebase from "firebase/app";
+// import initFirebase from "../components/firebase";
+// initFirebase();
+// const db = firebase.database();
 
-const Biltong = () => {
+const backorder = () => {
+  const [modalIsOpen, setModalIsOpen] = useState(false);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    emailjs
+      .sendForm(
+        "service_gbnx6zl",
+        "template_br67bbm",
+        e.target,
+        "user_lJcArVfFvxowZmvIrQxgV"
+      )
+      .then(
+        (result) => {
+          console.log(result.text);
+        },
+        (error) => {
+          console.log(error.text);
+        }
+      );
+    e.target.reset();
+    setModalIsOpen(true);
+
+    // const backorderList = db.ref("backorderList");
+    // const newBackorderList = backorderList.push();
+    // newBackorderList
+    //   .set({
+    //     name: isBackorder.name,
+    //     email: isBackorder.email,
+    //     phone: isBackorder.phone,
+    //     address: isBackorder.address,
+    //     details: isBackorder.details,
+    //   })
+    //   .then(() => {
+    //     alert("Succsessful");
+    //   })
+    //   .catch((error) => {
+    //     alert(error.message);
+    //   });
+  };
+
   return (
     <React.Fragment>
       <Head>
@@ -18,34 +62,48 @@ const Biltong = () => {
           className="backorder__logo"
         />
         <h2 className="backorder__title">Backorder Form</h2>
-        {/* <p className="backorder__description">
-          Thank you for your interest. We are currently out of stock but will
-          still accept orders and promise to deliver as soon as the next batch
-          is ready. Please provide your contact details below.
-        </p> */}
-        <form className="backorder__form">
+
+        <form className="backorder__form" onSubmit={handleSubmit}>
           <div className="inputBox_effect1">
-            <input type="text" required />
+            <input type="text" required name="name" />
             <span>Name</span>
           </div>
           <div className="inputBox_effect1">
-            <input type="text" required />
+            <input type="text" required name="email" />
             <span>Email</span>
           </div>
           <div className="inputBox_effect1">
-            <input type="text" required />
+            <input type="text" required name="phone" />
             <span>Phone</span>
           </div>
           <div className="inputBox_effect1">
-            <input type="text" required />
+            <input type="text" required name="address" />
             <span>Delivery Address</span>
           </div>
           <div className="inputBox_effect1">
-            <textarea type="text" required />
+            <textarea type="text" required name="details" />
             <span>Order details</span>
           </div>
           <button className="button__lightPrimary">Submit</button>
         </form>
+        <Modal
+          isOpen={modalIsOpen}
+          onRequestClose={() => setModalIsOpen(false)}
+          ariaHideApp={false}
+          className="modal"
+        >
+          <img src="/tick.svg" alt="tick icon" />
+          <strong>Success!!!</strong>
+          <p>Thank you for your order. We will ship as soon as possible</p>
+          <button
+            onClick={() => setModalIsOpen(false)}
+            className="button_light--black"
+          >
+            <p>
+              Continue <span>❯</span>
+            </p>
+          </button>
+        </Modal>
         {/* <p className="copyrightNotice backorder__copyrightNotice ">
           Copyright @ Bilpo 2021
         </p> */}
@@ -54,4 +112,4 @@ const Biltong = () => {
   );
 };
 
-export default Biltong;
+export default backorder;
