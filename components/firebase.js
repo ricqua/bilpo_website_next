b6a09a8ce0c13f7442963f54ef5d34ac7cc763e6
Ricqua <ricqua@gmail.com>
2021-04-19 21:01:56 +0900

    add firebase env details to vercel

--- components/firebase.js
+++ components/firebase.js
@@ -3,14 +3,10 @@ import "firebase/auth";
 import "firebase/database";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBMERrQ6jTwgUr3t4p8GiZcA69ItrEd5ow",
-  authDomain: "bilpowebsiteav1.firebaseapp.com",
-  databaseURL: "https://bilpowebsiteav1-default-rtdb.firebaseio.com",
-  projectId: "bilpowebsiteav1",
-  // apiKey: process.env.NEXT_PUBLIC_FIREBASE_PUBLIC_API_KEY,
-  // authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTHDOMAIN,
-  // databaseURL: process.env.NEXT_PUBLIC_FIREBASE_DB_URL,
-  // projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_PUBLIC_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTHDOMAIN,
+  databaseURL: process.env.NEXT_PUBLIC_FIREBASE_DB_URL,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
 };
 
 function initFirebase() {
